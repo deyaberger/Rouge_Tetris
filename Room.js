27985c03ff2155d22adb29aadae540c8e667f71c
Deya Berger <deyaberger@MacBook-Air-de-Deya.local>
2022-03-23 10:56:16 +0100

    avec Room management

--- Room.js
+++ Room.js
@@ -1,14 +1,34 @@
-class Room {
-	constructor(data) {
-		console.log("COUCOUC")
-		this.data = JSON.parse(data);
-	  this.name = this.data["room"];
-	  console.log("DATA " + data);
-	  console.log("DATA ROOM " + this.data.room);
-	  console.log("NAME " + this.name);
+class AllRooms {
+	constructor() {
+		this.count = 0;
+		this.rooms_list = [];
+		}
+	
+	add(room) {
+		this.count += 1;
+		this.rooms_list.push(room);
 	}
+
+	exists(name) {
+		let exists = false;
+		this.rooms_list.forEach(element => {
+			if (element.name == name) {
+				exists = true };
+		})
+		return exists;
+	}
+}
+
+class Room {
+	constructor(data, id) {
+		this.id = id;
+		this.name = data.room_name;
+		this.master = data.player_name
+		this.players = []
+	}
+
   }
 
-  module.exports = {Room}
+  module.exports = {AllRooms, Room}
 
   
--- index.js
+++ index.js
@@ -3,21 +3,31 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const { Room } = require('./Room');
+const { AllRooms, Room } = require('./Room');
 const io = new Server(server);
 
 // app.get('/', (req, res) => {
 //   res.sendFile(__dirname + '/index.html');
 // });
 
+const all_rooms = new AllRooms();
+
+const room_manager = function(msg, all_rooms) {
+	if (all_rooms.exists(msg.room_name) == false) {
+		console.log("Creating Room [" + msg.room_name + "]")
+		const room = new Room(msg, all_rooms.count);
+		all_rooms.add(room);
+	}
+	else {
+		console.log("Room [" + msg.room_name + "] already exists!")
+	}
+} 
+
 io.on('connection', (socket) => {
 	socket.on('room', (msg) => {
-		const data = JSON.stringify(msg);
-		console.log("MESSAGE: ")
-		console.log(msg);
-		const myroom = new Room(msg);
-	  console.log('room: ' + data);
-	  console.log("room name :" + myroom.name)
+		room_manager(msg, all_rooms);
+		console.log();
+		console.log(all_rooms);
 	});
   });
 
