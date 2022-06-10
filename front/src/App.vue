efc8b5309b59aeebb961225bf0eed2235bc549ac
Lewis <lewis.bonnetete@gmail.com>
2022-06-10 09:28:50 +0200

    front test

--- front/src/App.vue
+++ front/src/App.vue
@@ -14,19 +14,16 @@ export default defineComponent({
   },
   methods: {
     disconnect() {
-      console.error('disconnect');
       this.$store.dispatch('game/quit');
       this.$store.dispatch('error/clear');
       this.$router.push('/');
     },
     error() {
-      console.error('connect_error');
       this.$store.dispatch('game/quit');
       this.$store.dispatch('error/clear');
       this.$router.push('/error');
     },
     failed() {
-      console.error('connect_failed');
       this.$store.dispatch('game/quit');
       this.$store.dispatch('error/clear');
       this.$router.push('/error');
